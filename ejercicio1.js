36e1909345f649517f51cd0259a0063c9ffee73c
Snipsx17 <ing.uberthhernandez@gmail.com>
2023-08-07 17:40:58 +0200

    primer ejercicio

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+// Crea  un  archivo  ejercicio1.js  que  tenga  un  objeto  con  los  siguientes  campos:  Nombre, apellidos,  un  array  con  los  temas  del  bootcamp  que  ya  conoces,  si  estás  en  busqueda activa  con  un  boolean  y  un  array  de  objetos  el  cual  tenga  un  link  a  alguna  red  social  con  el nombre  y  link  de  la  red  social.  (Con  uno  vale,  Github  por  ejemplo,  pero  dentro  de  un  array).
+
+const user = {
+    nombre: 'Uberth',
+    apellidos: 'hernandez perez',
+    temas: [
+        'html',
+        'css',
+        'javscript'
+    ],
+    enBusqueda: true,
+    rrss: [
+        {
+            github: 'https://github.com/Snipsx17',
+            linkedin: 'https://www.linkedin.com/in/uberth-hernandez-perez-31815b146/'
+        }
+    ]
+}
